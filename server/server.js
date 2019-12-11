0f4842c545510ad0bd3810f613f6aaf71c560aae
Strider Wilson <Strider@Striders-MacBook-Pro.local>
2019-12-10 16:07:15 -0800

    Set up static proxy server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const proxy = require('http-proxy-middleware');
+const app = express();
+const port = 3000;
+
+// const apiProxy = proxy('/api')
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
